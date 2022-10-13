6a0087389e655b295cc551129dcb74bd21b31bd9
Marc Bouvier <m.bouvier.dev@gmail.com>
2022-10-13 22:36:14 +0200

    tdd: begin version verification

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,7 +4,6 @@ import {
 } from "https://deno.land/std@0.159.0/testing/asserts.ts";
 import {InvalidVCardContent, VCard} from "./index.ts";
 
-
 function parseVCard(rawContent: string): VCard {
   return new VCard(rawContent)
 }
@@ -13,6 +12,20 @@ Deno.test("VCard", async (t) => {
 
   await t.step("General Properties", async (generalProperties) => {
 
+    await generalProperties.step("constructs with the minimal required properties", () => {
+
+      assertObjectMatch(parseVCard("BEGIN:VCARD\n" +
+        "VERSION:4.0\n" +
+        "END:VCARD\n"), {});
+      assertObjectMatch(parseVCard("begin:vcard\n" +
+        "VERSION:4.0\n" +
+        "END:VCARD\n"), {});
+      assertObjectMatch(parseVCard("BEGIN:VCARd\n" +
+        "VERSION:4.0\n" +
+        "END:VCARD\n"), {});
+    })
+
+    // https://datatracker.ietf.org/doc/html/rfc6350#section-6.1.1
     await generalProperties.step("throws when it does not begin with BEGIN:VCARD", () => {
 
       assertThrows(() => parseVCard(""),
@@ -30,19 +43,7 @@ Deno.test("VCard", async (t) => {
 
     })
 
-    await generalProperties.step("begins with case-insentitive 'BEGIN:VCARD'", () => {
-
-      assertObjectMatch(parseVCard("BEGIN:VCARD\n" +
-        "\n" +
-        "END:VCARD\n"), {});
-      assertObjectMatch(parseVCard("begin:vcard\n" +
-        "\n" +
-        "END:VCARD\n"), {});
-      assertObjectMatch(parseVCard("BEGIN:VCARd\n" +
-        "\n" +
-        "END:VCARD\n"), {});
-    })
-
+    // https://datatracker.ietf.org/doc/html/rfc6350#section-6.1.2
     await generalProperties.step("throws when it does not end with END:VCARD", () => {
 
       assertThrows(() => parseVCard("BEGIN:VCARD\n" +
@@ -52,11 +53,44 @@ Deno.test("VCard", async (t) => {
         "with a value of \"VCARD\". The value is case-insensitive.");
 
       assertThrows(() => parseVCard("BEGIN:VCARD\n" +
-          "\n" +
+          "VERSION:4.0\n" +
           "END:VCAR\n"),
         InvalidVCardContent,
         "The content entity MUST end with the END type " +
         "with a value of \"VCARD\". The value is case-insensitive.");
+
+    })
+
+    // https://datatracker.ietf.org/doc/html/rfc6350#section-3.3
+    await generalProperties.step("throws when VERSION is not present", () => {
+
+      assertThrows(() => parseVCard("BEGIN:VCARD\n" +
+          "\n" +
+          "END:VCARD\n"),
+        InvalidVCardContent,
+        "A vCard object MUST include the VERSION property.");
+
+    })
+
+    // https://datatracker.ietf.org/doc/html/rfc6350#section-3.3
+    await generalProperties.step(
+      "throws when VERSION:4.0 does not come immediately after BEGIN:VCARD", () => {
+
+        assertThrows(() => parseVCard("BEGIN:VCARD\n" +
+            "SOMETHINNG\n" +
+            "VERSION:4.0\n" +
+            "END:VCARD\n"),
+          InvalidVCardContent,
+          "VERSION:4.0 MUST come immediately after BEGIN:VCARD.");
+
+      })
+
+    // https://datatracker.ietf.org/doc/html/rfc6350#section-6.7.9
+    // https://en.wikipedia.org/wiki/VCard
+    await generalProperties.step("throws when VERSION is not a correct version", () => {
+      // 2.1
+      // 3.0
+      // 4.0
 
     })
   });
--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,24 @@ export class VCard {
         "The content entity MUST end with the END type " +
         "with a value of \"VCARD\". The value is case-insensitive.")
     }
+    if (!VCard.containsVersion(rawContent)) {
+      throw new InvalidVCardContent(
+        "A vCard object MUST include the VERSION property.")
+    }
+    if (VCard.containsVersion4_0(rawContent)) {
+      if (!VCard.containsVersion4_0JustAfterBegin(rawContent)) {
+        throw new InvalidVCardContent(
+          "VERSION:4.0 MUST come immediately after BEGIN:VCARD.")
+      }
+    }
+
+  }
+
+  private static containsVersion(rawContent: String) {
+    return rawContent
+      .toLowerCase()
+      .split("\n")
+      .some(it => it.startsWith("version:"));
   }
 
   private static startsWithBegin(rawContent: String): boolean {
@@ -34,4 +52,21 @@ export class VCard {
       .endsWith("end:vcard");
   }
 
+  private static containsVersion4_0JustAfterBegin(rawContent: String) {
+    const beginIndex = rawContent.toLowerCase()
+      .split('\n')
+      .findIndex((value) => value === "begin:vcard")
+    const version4_0Index = rawContent.toLowerCase()
+      .split('\n')
+      .findIndex((value) => value === "version:4.0")
+
+    return version4_0Index === (beginIndex + 1)
+  }
+
+  private static containsVersion4_0(rawContent: String) {
+    return rawContent
+      .toLowerCase()
+      .split('\n')
+      .includes("version:4.0");
+  }
 }
